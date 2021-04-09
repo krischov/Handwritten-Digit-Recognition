e71551cfae14ef93c3fd85548ae68a21c94f5cc1
uoa-kcho598 <kcho598@aucklanduni.ac.nz>
2021-04-09 14:25:33 +1200

    Update main.py
    Added grayscale transform to make the image 3 channels.

--- Scripts/main.py
+++ Scripts/main.py
@@ -21,7 +21,11 @@ from torch import nn, optim
 
 def initAndLoadMNIST():
 
-    datasetTransform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5),(0.5))])
+    datasetTransform = transforms.Compose([
+        transforms.Grayscale(num_output_channels=3),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
     TRAIN = datasets.MNIST(root = 'Data\TrainData', train = True, transform = datasetTransform, download = False)
     TEST = datasets.MNIST(root = 'Data\TestData', train = False, transform = datasetTransform, download = False)
 
